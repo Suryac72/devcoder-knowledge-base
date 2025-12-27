6b8b9c1cf47bb92707bd999a1699369e289eb70f
Surya Prakash Chaudhary <suryac72@gmail.com>
2025-12-27 19:55:57 +0530

    fix:search

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,17 +1,7 @@
-import { source } from "@/lib/source";
-import { createSearchAPI } from "fumadocs-core/search/server";
+import { source } from '@/lib/source';
+import { createFromSource } from 'fumadocs-core/search/server';
 
-// Make this route static so Next.js can include it during `output: 'export'` builds
-export const dynamic = "force-static";
-
-export const { GET } = createSearchAPI("advanced", {
-  language: "english",
-  indexes: source.getPages().map((page) => ({
-    title: page.data.title,
-    description: page.data.description,
-    url: page.url,
-    id: page.url,
-    structuredData: page.data.structuredData,
-    tag: "<value>",
-  })),
+export const { GET } = createFromSource(source, {
+  // https://docs.orama.com/docs/orama-js/supported-languages
+  language: 'english',
 });
